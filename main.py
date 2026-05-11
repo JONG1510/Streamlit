772c1e234237f853c08870565b9b36a087d3cbf0
JONG15 <87769421+JONG1510@users.noreply.github.com>
2026-05-11 11:31:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.title("My First Streamlit App")
-st.write("Hello jackass! This is running from VS Code.")
+st.write("Hello World! This is running from VS Code.")
 
 # Add a simple slider
 number = st.slider("Select a number", 0, 100, 20)
